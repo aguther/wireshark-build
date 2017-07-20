078d2a0f1a7e2d14361ee47e5057f2db59fe830f
Andreas Guther <andreas@guther.net>
2017-07-20 23:35:57 +0200

    Install git instead of git2u

--- wireshark-prepare.sh
+++ wireshark-prepare.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
 # install needed packages
-sudo yum install -y gcc gcc-c++ bison flex libpcap-devel qt-devel gtk3-devel rpm-build libtool c-ares-devel qt5-qtbase-devel qt5-qtmultimedia-devel qt5-linguist desktop-file-utils libgcrypt-devel lz4-devel snappy-devel libnghttp2-devel libxml2-devel
-
-# install git2u
-sudo yum install -y git2u
-
-# install git
-#sudo yum install git
+sudo yum install -y git gcc gcc-c++ bison flex libpcap-devel qt-devel gtk3-devel rpm-build libtool c-ares-devel qt5-qtbase-devel qt5-qtmultimedia-devel qt5-linguist desktop-file-utils libgcrypt-devel lz4-devel snappy-devel libnghttp2-devel libxml2-devel
