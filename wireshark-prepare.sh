393ebf7a879621cd7261a00f68b8854649bf0432
Andreas Guther <andreas@guther.net>
2017-07-20 23:55:26 +0200

    Fixed issues with libraries and improved format of file

--- wireshark-prepare.sh
+++ wireshark-prepare.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
 
+# install epel
+yum install -y epel-release
+
 # install needed packages
-sudo yum install -y git gcc gcc-c++ bison flex libpcap-devel qt-devel gtk3-devel rpm-build libtool c-ares-devel qt5-qtbase-devel qt5-qtmultimedia-devel qt5-linguist desktop-file-utils libgcrypt-devel lz4-devel snappy-devel libnghttp2-devel libxml2-devel
+yum install -y \
+    bison \
+    c-ares-devel \
+    desktop-file-utils \
+    flex \
+    gcc \
+    gcc-c++ \
+    git \
+    gtk3-devel \
+    libgcrypt-devel \
+    libnghttp2-devel \
+    libpcap-devel \
+    libtool \
+    libxml2-devel
+    lz4-devel \
+    qt-devel \
+    qt5-linguist \
+    qt5-qtbase-devel \
+    qt5-qtmultimedia-devel \
+    rpm-build \
+    snappy-devel
