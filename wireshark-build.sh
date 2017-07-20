6a322eee186129cbbe0948f63c76dfdf0681824d
Andreas Guther <andreas@guther.net>
2017-07-20 23:04:20 +0200

    Initial version of scripts to build wireshark on RHEL7

---
+++ wireshark-build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# defaults
+WIRESHARK_VERSION_TAG=v2.4.0
+WIRESHARK_BUILD_DIRECTORY=/tmp/wireshark
+
+# ensure directory is not present
+rm -rf ${WIRESHARK_BUILD_DIRECTORY}
+
+# get git repository
+git clone https://code.wireshark.org/review/wireshark ${WIRESHARK_BUILD_DIRECTORY}
+
+# change to git repository
+pushd ${WIRESHARK_BUILD_DIRECTORY}
+
+# get tag
+git checkout tags/${WIRESHARK_VERSION_TAG}
+
+# generate build files
+./autogen.sh
+
+# configure
+./configure
+
+# build
+make rpm-package
+
+# restore directory
+popd
