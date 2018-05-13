d1f39740f6959f7e7432a15ae87167b2ee7db958
Andreas Guther <andreas@guther.net>
2018-05-13 22:04:09 +0200

    Removed specific versions from packages

--- wireshark-prepare.sh
+++ wireshark-prepare.sh
@@ -11,20 +11,21 @@ yum erase -y git perl-Git
 
 # install needed packages
 yum install -y \
-    bison-2.7-4.el7 \
-    c-ares-devel-1.10.0-3.el7 \
-    desktop-file-utils-0.22-1.el7 \
-    flex-2.5.37-3.el7 \
-    gcc-4.8.5-4.el7 \
-    gcc-c++-4.8.5-4.el7 \
+    automake \
+    bison \
+    c-ares-devel \
+    desktop-file-utils \
+    flex \
+    gcc \
+    gcc-c++ \
     git2u \
-    gtk3-devel-3.14.13-16.el7 \
-    libgcrypt-devel-1.5.3-12.el7_1.1 \
+    gtk3-devel \
+    libgcrypt-devel \
     libnghttp2-devel \
     libpcap-devel \
-    libtool-2.4.2-20.el7 \
-    libxml2-devel-2.9.1-5.el7_1.2 \
+    libtool \
+    libxml2-devel \
     lz4-devel \
-    qt-devel-4.8.5-11.el7 \
-    rpm-build-4.11.3-17.el7 \
+    qt-devel \
+    rpm-build \
     snappy-devel
